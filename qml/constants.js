f9ee285a0cb479b65f88721d43801b3d65a09b6d
Victor Dudochkin <dudochkin.victor@gmail.com>
2013-05-15 14:54:35 +0800

    Initial commit

---
+++ qml/constants.js
@@ -0,0 +1,29 @@
+// Constants file to hold all constants used in Socialweb.qml
+
+var titleText = qsTr("Web accounts");
+
+var dialogButtonText = qsTr("Done");
+var dialogTitleText = qsTr("Sign in");
+
+var signInText = qsTr("Sign in");
+var signOutText = qsTr("Sign out");
+var continueText = qsTr("Continue");
+var buttonHeight = 60;
+var buttonWidth = 150;
+var usernameText = qsTr("<b>Username:</b> ");
+var usernameDefaultText = qsTr("Username");
+var passwordDefaultText = qsTr("Password");
+var verifierDefaultText = qsTr("Verifier code");
+var linkText = "<br /><a href='%1'>%2</a>";
+var moreDetailsText = qsTr("More details");
+
+var cantSignInText = qsTr("Unable to sign in, try again later");
+var signingInText = qsTr("Attempting to sign in...");
+var signedInText = qsTr("Successfully signed in!");
+var signedOutText = qsTr("You have successfully signed out.  Content from this service will no longer appear on this device");
+
+var iconPath = "image://meegotheme/icons/services/";
+var iconPathGeneric = "generic";
+var nameParam = "user";
+var passwordParam = "password";
+
